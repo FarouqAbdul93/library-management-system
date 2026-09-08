12c21a0d7b951f0d0bdb881e64e8485bb7699343
Farouq <farouqha93@gmail.com>
2026-09-08 12:21:26 +0100

    Add Book class with constructor, test-first (Red-Green)

---
+++ LibraryManagementSystem/LibraryManagementSystem.Tests/BookTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using LibraryManagementSystem;
+
+namespace LibraryManagementSystem.Tests
+{
+    public class BookTests
+    {
+        [Test]
+        public void Constructor_SetsTitleAndAuthor_AndBookIsNotBorrowed()
+        {
+            // Arrange & Act
+            var book = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling");
+
+            // Assert
+            Assert.That(book.Title, Is.EqualTo("Harry Potter and the Philosopher's Stone"));
+            Assert.That(book.Author, Is.EqualTo("J.K. Rowling"));
+            Assert.That(book.IsBorrowed, Is.False);
+        }
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public bool IsBorrowed { get; private set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            IsBorrowed = false;
+        }
+    }
+}
